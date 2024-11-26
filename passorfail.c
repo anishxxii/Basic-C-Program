5992419dc6477be25fc9a927e0702f291bb606ef
Anish <anishiitmbs@gmail.com>
2024-11-26 22:07:32 +0530

    Add files via upload

---
+++ passorfail.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    printf("Enter marks (0-100): ");
+    scanf("%d", &marks);
+
+    if (marks >= 0 && marks < 30) {
+        printf("Fail\n");
+    } else if (marks >= 30 && marks <= 100) {
+        printf("Pass\n");
+    } else {
+        printf("Invalid input. Please enter a number between 0 and 100.\n");
+    }
+
+    return 0;
+}
